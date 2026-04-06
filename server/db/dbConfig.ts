01a3d65253b015d126fd2dbc67305b1973d34fdc
Priyadharsh1ni <priyariya1261@gmail.com>
2026-04-06 13:55:06 +0530

    fix: parse DB_PORT to ensure it defaults to 3306 if not set

--- server/db/dbConfig.ts
+++ server/db/dbConfig.ts
@@ -5,7 +5,7 @@ const pool = mysql.createPool({
   user: process.env.DB_USER as string,
   password: process.env.DB_PASSWORD as string,
   database: process.env.DB_NAME as string,
-  port: process.env.DB_PORT as number, 
+  port: parseInt(process.env.DB_PORT || '3306', 10),
   url: process.env.DB_URL as string
 
 });

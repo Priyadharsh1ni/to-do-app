dd3fa596abd849a2212542a5da4bb8235435a930
Priyadharsh1ni <priyariya1261@gmail.com>
2026-04-06 12:14:08 +0530

    feat: update environment configuration and add dotenv dependency

--- server/db/dbConfig.ts
+++ server/db/dbConfig.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const dbConfig = {
-  host: 'localhost',
-  user: 'root',
-  password: 'Password',
-  database: 'todo',
-  port: 3306,
+  host: process.env.DB_HOST || 'localhost',
+  user: process.env.DB_USER || 'root',
+  password: process.env.DB_PASSWORD || 'Password',
+  database: process.env.DB_NAME || 'todo',
+  port: process.env.DB_PORT || 3306,
 };  
 
 
--- server/index.ts
+++ server/index.ts
@@ -4,7 +4,7 @@ import bodyParser from "body-parser";
 import taskRoutes from "./router/tasks.ts"; 
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(bodyParser.json());
